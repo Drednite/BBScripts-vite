eacf144ddb9c76fb15f071291a9cf3e90e577514
Drednite <drednite@live.com>
2023-12-01 12:52:31 -0600

    Fixed serverHasher and added propper logging.

--- src/serverHasher.ts
+++ src/serverHasher.ts
@@ -8,6 +8,9 @@ export function autocomplete(data: AutocompleteData) {
 export async function main(ns: NS): Promise<void> {
   const target = ns.args[0].toString();
   const net = ns.hacknet;
+  ns.disableLog('ALL');
+  ns.enableLog('hacknet.spendHashes');
+  ns.tail();
 
   while (true) {
     if (
@@ -15,14 +18,19 @@ export async function main(ns: NS): Promise<void> {
       net.hashCost('Reduce Minimum Security') < net.hashCost('Increase Maximum Money')
     ) {
       await upgradeHashCapacity(ns, net.hashCost('Reduce Minimum Security'));
+      ns.print('Reducing ' + target + ' security...');
       while (!net.spendHashes('Reduce Minimum Security', target)) {
         await ns.sleep(1000);
       }
+      ns.printf('Target security reduced to %d', ns.getServerMinSecurityLevel(target));
     } else if (ns.getServerMaxMoney(target) < 10 ** 13) {
       await upgradeHashCapacity(ns, net.hashCost('Increase Maximum Money'));
+      ns.print('Increasing ' + target + ' capacity...');
       while (!net.spendHashes('Increase Maximum Money', target)) {
         await ns.sleep(1000);
       }
+      ns.print('Target capacity increased to ' + ns.formatNumber(ns.getServerMaxMoney(target)));
     }
+    await ns.sleep(1000);
   }
 }
