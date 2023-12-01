b8a5f662b7e2247636b8904dacad36f0ed70f1aa
Drednite <drednite@live.com>
2023-12-01 13:40:50 -0600

    updated to have end condition & placement

--- src/serverHasher.ts
+++ src/serverHasher.ts
@@ -11,19 +11,19 @@ export async function main(ns: NS): Promise<void> {
   ns.disableLog('ALL');
   ns.enableLog('hacknet.spendHashes');
   ns.tail();
+  ns.moveTail(250, 0);
+  ns.resizeTail(515, 300);
 
-  while (true) {
-    if (
-      ns.getServerMinSecurityLevel(target) > 1 &&
-      net.hashCost('Reduce Minimum Security') < net.hashCost('Increase Maximum Money')
-    ) {
+  while (ns.getServerMinSecurityLevel(target) > 1 || ns.getServerMaxMoney(target) < 10 ** 13) {
+    if (ns.getServerMinSecurityLevel(target) > 1) {
       await upgradeHashCapacity(ns, net.hashCost('Reduce Minimum Security'));
       ns.print('Reducing ' + target + ' security...');
       while (!net.spendHashes('Reduce Minimum Security', target)) {
         await ns.sleep(1000);
       }
       ns.printf('Target security reduced to %d', ns.getServerMinSecurityLevel(target));
-    } else if (ns.getServerMaxMoney(target) < 10 ** 13) {
+    }
+    if (ns.getServerMaxMoney(target) < 10 ** 13) {
       await upgradeHashCapacity(ns, net.hashCost('Increase Maximum Money'));
       ns.print('Increasing ' + target + ' capacity...');
       while (!net.spendHashes('Increase Maximum Money', target)) {
