ec20020e876b6d764360856c16502b7059af1dce
Drednite <drednite@live.com>
2023-12-07 21:37:06 -0600

    Added forecast indicators

--- src/stockReporter.ts
+++ src/stockReporter.ts
@@ -1,7 +1,18 @@
 import { AutocompleteData, NS } from '@ns';
 import { Color, colorPicker } from './helpers';
 
-const argsSchema: [string, string | number | boolean | string[]][] = [['width', 36]];
+const argsSchema: [string, string | number | boolean | string[]][] = [['width', 90]];
+const arrows = [
+  colorPicker('⟱', Color.magenta),
+  colorPicker('⤋', Color.magenta),
+  colorPicker('⇓', Color.magenta),
+  colorPicker('↓', Color.white),
+  colorPicker('↕', Color.white),
+  colorPicker('↑', Color.white),
+  colorPicker('⇑', Color.green),
+  colorPicker('⤊', Color.green),
+  colorPicker('⟰', Color.green),
+];
 
 export function autocomplete(data: AutocompleteData) {
   data.flags(argsSchema);
@@ -12,8 +23,26 @@ export function autocomplete(data: AutocompleteData) {
 export async function main(ns: NS) {
   const flags = ns.flags(argsSchema);
   const st = ns.stock;
+  if (!st.hasWSEAccount && !st.hasTIXAPIAccess) {
+    ns.tprint('ERROR: Needs WSE Account and TIX API Access');
+    ns.exit();
+  }
+  let has4S = false;
+  if (st.has4SDataTIXAPI()) {
+    has4S = true;
+  }
   const dataFile = 'data/stocks.txt';
   const stocks: Stock[] = [];
+  const legend =
+    '|Long:   ' +
+    colorPicker('▓░', Color.blue) +
+    '|\n|Short:  ' +
+    colorPicker('▓░', Color.magenta) +
+    '|\n|Profit: ' +
+    colorPicker('▓░', Color.green) +
+    '|\n|Loss:   ' +
+    colorPicker('▓░', Color.red) +
+    '|';
   class Stock {
     name: string;
     highestPrice: number;
@@ -67,7 +96,7 @@ export async function main(ns: NS) {
       stocks.push(new Stock(sym));
     }
   }
-  let rowSize = 90;
+  let rowSize = 34;
   if (typeof flags.width == 'number') {
     rowSize = Math.max(flags.width, 34);
   }
@@ -76,23 +105,41 @@ export async function main(ns: NS) {
   const height = columnSize * 24 + 62;
   await st.nextUpdate();
   ns.tail();
+  ns.setTitle(ns.getScriptName());
   while (true) {
     ns.resizeTail(width, height);
     ns.clearLog();
+    ns.printf("┌%'─10s┐", '');
+    ns.print(legend);
+    ns.printf("└%'─10s┘", '');
     ns.printf("┌%'─6s─┬─Lowest%'─" + (rowSize - 18) + 's┐', 'Stock', 'Highest');
     for (const stock of stocks) {
+      let tend = '│';
+      if (has4S) {
+        const forecast = st.getForecast(stock.name);
+        if (forecast <= 0.3) tend = arrows[0];
+        else if (forecast <= 0.35) tend = arrows[1];
+        else if (forecast <= 0.4) tend = arrows[2];
+        else if (forecast <= 0.45) tend = arrows[3];
+        else if (forecast <= 0.55) tend = arrows[4];
+        else if (forecast <= 0.6) tend = arrows[5];
+        else if (forecast <= 0.65) tend = arrows[6];
+        else if (forecast <= 0.7) tend = arrows[7];
+        else tend = arrows[8];
+      }
       stock.Update();
       const perc = (stock.currentPrice - stock.lowestPrice) / (stock.highestPrice - stock.lowestPrice);
       const position =
         (Math.max(stock.currentPosition[1], stock.currentPosition[3]) - stock.lowestPrice) /
         (stock.highestPrice - stock.lowestPrice);
-      const bar = makeBar(rowSize - 30, perc, position);
+      const bar = makeBar(rowSize - 32, perc, position, stock.currentPosition[3] > 0);
       ns.printf(
-        '│ %5s │ %8s %s %8s│',
+        '│ %5s | %8s %s %8s%s',
         stock.name,
         '$' + ns.formatNumber(stock.lowestPrice, 2),
-        bar,
+        '[' + bar + ']',
         '$' + ns.formatNumber(stock.highestPrice, 2),
+        tend,
       );
     }
     ns.printf("└%'─7s┴%'─-" + (rowSize - 11) + 's┘', '', '');
@@ -101,25 +148,34 @@ export async function main(ns: NS) {
   }
 }
 
-function makeBar(size: number, percent: number, position?: number) {
-  let bar = '[';
+function makeBar(size: number, percent: number, position?: number, short?: boolean) {
+  let bar = '';
   if (!position) {
-    position = 0;
+    position = -1;
   }
   let pos = 0;
-  for (let i = 1; i < size - 1; i++) {
-    if (position > i / size) {
+  for (let i = 0; i < size; i++) {
+    if (position >= (i / size + (i + 1) / size) / 2) {
       pos = i;
     }
-    if (percent < i / size) {
+    if (percent <= (i / size + (i + 1) / size) / 2) {
       bar += '░';
     } else {
       bar += '▓';
     }
   }
-  bar += ']';
   if (position > 0) {
-    bar = bar.slice(0, pos) + colorPicker(bar.charAt(pos), Color.cyan) + bar.slice(pos + 1);
+    if (short) {
+      bar =
+        bar.slice(0, pos).replaceAll('░', colorPicker('░', Color.green)) +
+        colorPicker(bar.charAt(pos), Color.magenta) +
+        bar.slice(pos + 1).replaceAll('▓', colorPicker('▓', Color.red));
+    } else {
+      bar =
+        bar.slice(0, pos).replaceAll('░', colorPicker('░', Color.red)) +
+        colorPicker(bar.charAt(pos), Color.blue) +
+        bar.slice(pos + 1).replaceAll('▓', colorPicker('▓', Color.green));
+    }
   }
   return bar;
 }
